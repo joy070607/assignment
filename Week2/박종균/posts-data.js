acd81534a0f1b39e74d2e39644a963eae14ee2cb
joy070607 <137891285+joy070607@users.noreply.github.com>
2026-04-02 21:08:24 +0900

    [Week2] 박종균 과제 제출

---
+++ Week2/박종균/posts-data.js
@@ -0,0 +1,73 @@
+// 라이브 코딩: 아래 배열에 글 객체를 채워 넣습니다. (시작은 빈 배열)
+const posts = [
+    // 글 한 개 = 객체 하나
+    {
+        id:1,
+        category:"공지",
+        title:"프론트엔드 스터디 1주차 안내",
+        summary:"HTML과 CSS를 할용해 게시글 상세 페이지를 만들기",
+        author:"관리자",
+        date:"2026.04.02",
+        views:200
+    }
+,
+    {
+        id:2,
+        category:"질문",
+        title:"JS 관련해서 독학하려면 어떻게 공부하는 게 좋을까요?",
+        summary:"JS 배우고 싶은데 독학 방법을 모르겠어요ㅠㅠ",
+        author:"박스터디",
+        date:"2026.04.02",
+        views:189
+    }
+,
+    {
+        id:3,
+        category:"자유",
+        title:"프론트엔드 스터디 1주차 안내",
+        summary:"HTML과 CSS를 할용해 게시글 상세 페이지를 만들기",
+        author:"관리자",
+        date:"2026.04.02",
+        views:200
+    }
+,
+    {
+        id:4,
+        category:"질문",
+        title:"프론트엔드 스터디 1주차 안내",
+        summary:"HTML과 CSS를 할용해 게시글 상세 페이지를 만들기",
+        author:"관리자",
+        date:"2026.04.02",
+        views:200
+    }
+,
+    {
+        id:5,
+        category:"자유",
+        title:"프론트엔드 스터디 1주차 안내",
+        summary:"HTML과 CSS를 할용해 게시글 상세 페이지를 만들기",
+        author:"관리자",
+        date:"2026.04.02",
+        views:200
+    }
+,
+    {
+        id:6,
+        category:"자유",
+        title:"프론트엔드 스터디 1주차 안내",
+        summary:"HTML과 CSS를 할용해 게시글 상세 페이지를 만들기",
+        author:"관리자",
+        date:"2026.04.02",
+        views:200
+    }
+,
+    {
+        id:7,
+        category:"학습",
+        title:"프론트엔드 스터디 1주차 안내",
+        summary:"HTML과 CSS를 할용해 게시글 상세 페이지를 만들기",
+        author:"관리자",
+        date:"2026.04.02",
+        views:200
+    }
+];
